91135ef2c36d5e3799584a5e79c77227a72c70b9
sys_armando <patrickarmando87@gmail.com>
2021-11-13 00:15:31 +0900

    List Comprehension Last

--- comprehension_list.py
+++ comprehension_list.py
@@ -1,6 +1,6 @@
 print('\nList Daftar Buku')
 
-daftar_buku = ['Seven Habits','How to Influence','First Things','4DX']
+daftar_buku = ['Seven Habits', 'How to Influence', 'First Things', '4DX']
 for i in range(0, len(daftar_buku)):
     print(daftar_buku[i])
 
@@ -15,4 +15,20 @@ del daftar_buku[:]
 for i in range(0, len(daftar_buku)):
     print(daftar_buku[i])
 
+print('\nList Comprehension Start')
+daftar_buku = ['Seven Habits', 'How to Influence', 'First Things', '4DX']
+del daftar_buku[0:2]
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
 
+print('\nList Comprehension Start dari belakang')
+daftar_buku = ['Seven Habits', 'How to Influence', 'First Things', '4DX']
+del daftar_buku[0:-2]
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nList Comprehension Step')
+daftar_buku = ['Seven Habits', 'How to Influence', 'First Things', '4DX']
+del daftar_buku[0::2]
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
